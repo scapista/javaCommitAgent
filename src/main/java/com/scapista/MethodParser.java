dd68501689d1908d10342bdaca8313ee939c66a4
scapista <scapista5@gmail.com>
2018-02-04 02:28:49 -0500

    Features: Added java parsing that detects changes based on line number and returns the method(s) edits from the last commit.
    TODO:
    Add functionality to include results of test cases in commit file.

---
+++ src/main/java/com/scapista/MethodParser.java
@@ -0,0 +1,15 @@
+package com.scapista;
+
+/**
+ * Created by scapista on 2/4/18.
+ * +
+ * + Priority:
+ * + 1. build failing
+ * + 2. function not working
+ * + 3. nice to have but would optimize
+ * + 4. nice to have no benefit
+ * + 5. future state
+ */
+
+public class MethodParser {
+}
